2c81ddba4e029c21a290247b2e384ca10f55350c
Jeffrey Örnerstedt <jeffrey.ornerstedt@m.co>
2022-03-13 01:08:24 +0100

    Draw some more

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -28,32 +28,36 @@ export default function Canvas({ players, fruit }: Props) {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   function draw(context: CanvasRenderingContext2D) {
-    context.fillStyle = '#000000'
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    // Draw canvas
-    context.fillStyle = '#000000'
+    // Draw play board
+    context.fillStyle = '#1a1a1c'
     context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 
+    for (let index = 0; index <= context.canvas.width; index += 25) {
+      context.moveTo(0.5 + index, 0)
+      context.lineTo(0.5 + index + 0, context.canvas.height)
+    }
 
+    for (let index = 0; index <= context.canvas.height; index += 25) {
+      context.moveTo(0, 0.5 + index)
+      context.lineTo(context.canvas.width, 0.5 + index)
+    }
+
+    context.strokeStyle = '#24272d'
+    context.stroke()
 
     // Fraw fruit
     if (fruit) {
-      context.beginPath();
-      context.fillStyle = fruit.color;
-      context.arc(fruit.size/2 + fruit.position.x, fruit.position.y + fruit.size/2, fruit.size/2, 0, 2* Math.PI);
-      context.fill();
+      context.beginPath()
+      context.fillStyle = fruit.color
+      context.arc(fruit.size / 2 + fruit.position.x, fruit.position.y + fruit.size / 2, fruit.size / 2, 0, 2 * Math.PI)
+      context.fill()
     }
 
-       // Draw players
-       players.forEach((player) => {
-        context.beginPath();
-        context.fillStyle = player.color;
-        context.arc(player.size/2 + player.position.x, player.position.y + player.size/2, player.size/2, 0, 2* Math.PI);
-        context.fill();
-      })
-
-
-
+    // Draw players
+    for (let index = 0; index < players.length; index++) {
+      context.fillStyle = players[index].color
+      context.fillRect(players[index].position.x, players[index].position.y, players[index].size, players[index].size)
+    }
   }
 
   useEffect(() => {
