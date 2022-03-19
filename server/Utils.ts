83c357e17ffc0ea5c794c0f5d7dcc9f1bbdc2eb9
Jeffrey Örnerstedt <jeffrey.ornerstedt@m.co>
2022-03-19 21:25:57 +0100

    Breakout

--- server/Utils.ts
+++ server/Utils.ts
@@ -1,10 +1,12 @@
 import { playerSize, canvasSize } from './Constants'
-import { Fruit } from '../src/shared-types'
+import { PlayerDirection, Fruit } from '../src/shared-types'
 
 function hourTimeMs() {
   let time = process.hrtime()
   return time[0] * 1000 + time[1] / 1000000
 }
+
+const avalaibleDirections: PlayerDirection[] = ['Up', 'Right', 'Left', 'Down']
 
 const createPlayer = (id: string, color: string) => ({
   id,
@@ -13,7 +15,7 @@ const createPlayer = (id: string, color: string) => ({
   length: 1,
   position: { x: canvasSize / 2, y: canvasSize / 2 },
   positions: [],
-  direction: ['Up', 'Right', 'Left', 'Down'].sort(() => Math.random() - 0.5)[0],
+  direction: avalaibleDirections.sort(() => Math.random() - 0.5)[0],
 })
 
 const randomNum = () => Math.floor(Math.random() * 20) * 25
