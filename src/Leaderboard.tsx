90f970a7ef9fbbc0f3a915ef23b12d6145dc1fb6
Jeffrey Örnerstedt <jeffrey.ornerstedt@m.co>
2022-04-06 15:11:55 +0200

    No need to make Leaderboard stateful

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { Player } from './shared-types'
 
 interface Props {
@@ -7,25 +6,19 @@ interface Props {
 }
 
 export default function Leaderboard({ players, socketId }: Props) {
-  const [scores, setScores] = useState<Player[]>([])
-
-  useEffect(() => {
-    // Update players[] sort order by score val z-a
-
-    setScores(players.sort((a: Player, z: Player) => z.length - a.length))
-  }, [players])
-
   return (
     <>
-      {scores.map((player: Player) => (
-        <ul key={player.id} style={{ textAlign: 'left' }}>
-          <li>
-            {player.name}
-            {player.id === socketId && ' (You)'}
-          </li>
-          <li>Points: {player.length}</li>
-        </ul>
-      ))}
+      {[...players]
+        .sort((a: Player, z: Player) => z.length - a.length)
+        .map((player: Player) => (
+          <ul key={player.id} style={{ textAlign: 'left' }}>
+            <li>
+              {player.name}
+              {player.id === socketId && ' (You)'}
+            </li>
+            <li>Points: {player.length}</li>
+          </ul>
+        ))}
     </>
   )
 }
